01dc56806cdf99f5c2af69d610a23ebdd8a4c683
Victoria <vaajibade@gmail.com>
2025-07-21 23:00:19 +0200

    two sum algorithm

---
+++ two-sum.ts
@@ -0,0 +1,49 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+/*
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+// function twoSum(nums: number[], target: number): number[] {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j];
+//     }
+//   }
+//   return [];
+// }
+
+function twoSum(nums: number[], target: number): number[] {
+  const elements = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    // 1. get the diff btwn the cur value and the target
+    const difference = target - nums[i];
+    //2, check if the diff exist in the map
+    const secondIndex = elements.get(difference);
+    if (elements.has(difference)) {
+      return [elements.get(difference)!, i];
+    } else {
+      elements.set(nums[i], i);
+    }
+    //3. if not, add the cur value to the map and the index
+    //4 else return the current value index and the index of the diff in the map
+  }
+  return [];
+}
+
+// [2,7,11,15]
+[2];
